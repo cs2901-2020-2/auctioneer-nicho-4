eb2e77c0f921628887854cc00e4feb1253a303ac
Alejandro  Otero <alejandrotero@Alejandros-Air.lan>
2020-10-19 20:15:28 -0500

    Test cases in progress

--- src/test/java/Auction.java
+++ src/test/java/Auction.java
@@ -1,6 +1,15 @@
 import org.testng.annotations.Test;
+import cs.lab.Auctioneer;
+import cs.lab.Bidder;
+import cs.lab.Observer;
 
 @Test
 public class Auction {
 
+    @Test
+    public void test1(){
+        Auctioneer auctioneer = new Auctioneer();
+        Observer observer = new Observer();
+        auctioneer.registerObserver(observer);
+    }
 }
--- src/test/java/MultithreadAuction.java
+++ src/test/java/MultithreadAuction.java
@@ -3,5 +3,9 @@ import org.testng.annotations.Test;
 @Test
 public class MultithreadAuction {
 
+    @Test(invocationCount = 1000, threadPoolSize = 1000)
+    public void testMultithread(){
+        
+    }
 
 }
